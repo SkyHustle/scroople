8fa50259450741eb11afb888ecc5513c98b5471c
Dmitry Sky <dmitry.sky@icloud.com>
2016-05-20 16:51:12 -0600

    write model tests for user having email and password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "test user fixture exists" do
+  test "test user has correct email and password" do
+    # test_user attributes are setup in users.yml
     assert_equal "test_user_email@gmail.com", users(:test_user).email
+    assert_equal "test_user_password", users(:test_user).encrypted_password
   end
 end
