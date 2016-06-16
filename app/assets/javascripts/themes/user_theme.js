c5242aee190e497fb5a92ba3f5164bcd13be8e3f
Dmitry Sky <dmitry.sky@icloud.com>
2016-06-15 18:14:27 -0600

    propogation is kicking my ass real good but things seem to be moving along smoothly

--- app/assets/javascripts/themes/user_theme.js
+++ app/assets/javascripts/themes/user_theme.js
@@ -19,31 +19,38 @@ $(document).ready(function(){
     }
 
     $(".click-to-update").on("click", function(element){
+        event.stopPropagation();
         var innerContent   = $(this).text()
         var clickedElement = $(this)
 
         if($(".form-inline").size() !== 0){
+            event.stopPropagation()
             element.preventDefault()
         } else {
+            event.stopPropagation()
             $(this).after('<form class="form-inline"><div class="form-group"><input type="text" class="form-control" placeholder="'+innerContent+'" autofocus></div><button type="button" data-original-content="'+innerContent+'" class="btn btn-danger">Cancel</button><button type="button" class="btn btn-success">Submit</button></div>')
 
-            $("input").on("inputchange", function() {
+            $("input").on("inputchange", function(element) {
+                event.stopPropagation()
                 $(clickedElement).text(this.value)
             })
 
             $("input").on("keydown", function(key) {
+                event.stopPropagation()
                 if (key.keyCode === 13) {
                     key.preventDefault()
                     $(".btn-success").click()
                 }
             })
 
-            $(".btn-danger").on("click", function() {
+            $(".btn-danger").on("click", function(element) {
+                event.stopPropagation()
                 $(clickedElement).text($(this).data('original-content'))
                 $('.form-inline').remove()
             })
 
-            $(".btn-success").on("click", function() {
+            $(".btn-success").on("click", function(element) {
+                event.stopPropagation()
                 var themeID        = $("body").data("theme-id")
                 var themeColumn    = $(this).parent().prev().data("theme-column")
                 var newColumnValue = $(this).siblings().find("input").val()
@@ -57,6 +64,7 @@ $(document).ready(function(){
                         data: {agency: { [themeColumn]: newColumnValue} },
                         dataType: "json",
                         success: function(result) {
+                            event.stopPropagation()
                             console.log("success!", result.results)
                             $(".form-inline").remove()
                         }
