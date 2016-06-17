a007b9fdde431262656c045c49465567e18696f3
Dmitry Sky <dmitry.sky@icloud.com>
2016-06-17 16:44:03 -0600

    strictly identify clicked elements on the page by target.tagName and proceed accordingly

--- app/assets/javascripts/themes/user_theme.js
+++ app/assets/javascripts/themes/user_theme.js
@@ -1,3 +1,23 @@
+var editTextElement = function (themeID, themeColumn, newColumnValue) {
+    if (newColumnValue === ""){
+        console.log("empty input value")
+      return;
+    } else {
+        console.log("begin ajax call")
+        $.ajax({
+            url: "/themes/agency/" + themeID,
+            type: "PUT",
+            data: {agency: { [themeColumn]: newColumnValue} },
+            dataType: "json",
+            success: function(result) {
+                event.stopPropagation()
+                console.log("success!", result.results)
+                $(".form-inline").remove()
+            }
+        })
+    }
+}
+
 $(document).ready(function(){
     $.event.special.inputchange = {
         setup: function() {
@@ -18,60 +38,90 @@ $(document).ready(function(){
         }
     }
 
-    $(".click-to-update").on("click", function(element){
-        event.stopPropagation()
-        var innerContent   = $(this).text()
-        var clickedElement = $(this)
+    $("body").on("click", function() {
+        var textElements   = ["H1", "H2", "H3", "H4", "H5", "H6", "DIV", "P"]
+        var eventTargetTag = event.target.tagName
 
-        if($(".form-inline").size() !== 0){
-            event.stopPropagation()
-            element.preventDefault()
-        } else {
-            event.stopPropagation()
-            $(this).after('<form class="form-inline"><div class="form-group"><input type="text" class="form-control" placeholder="'+innerContent+'" autofocus></div><button type="button" data-original-content="'+innerContent+'" class="btn btn-danger">Cancel</button><button type="button" class="btn btn-success">Submit</button></div>')
+        if ($.inArray(eventTargetTag, textElements) >= 0
+            && !/\s{5,}/.test(event.target.textContent)) {
 
-            $("input").on("inputchange", function(element) {
-                // event.stopPropagation()
-                $(clickedElement).text(this.value)
-            })
+            var innerContent   = event.target.textContent
+            var clickedElement = event.target
 
-            $("input").on("keydown", function(key) {
-                event.stopPropagation()
-                if (key.keyCode === 13) {
-                    key.preventDefault()
-                    $(".btn-success").click()
-                }
-            })
+            if($(".form-inline").size() !== 0){
+                console.log("only one input field at a time!")
+                event.preventDefault()
+            } else {
+                console.log("begin adding input field")
+                $(clickedElement).after('<form class="form-inline"><div class="form-group"><input type="text" class="form-control" id="master-input" placeholder="'+innerContent+'" autofocus></div><button type="button" data-original-content="'+innerContent+'" class="btn btn-danger">Cancel</button><button type="button" class="btn btn-success">Submit</button></div>')
 
-            $(".btn-danger").on("click", function(element) {
-                event.stopPropagation()
-                $(clickedElement).text($(this).data('original-content'))
-                $('.form-inline').remove()
-            })
+                $("input").on("inputchange", function(element) {
+                    $(clickedElement).text(this.value)
+                })
 
-            $(".btn-success").on("click", function(element) {
-                event.stopPropagation()
-                var themeID        = $("body").data("theme-id")
-                var themeColumn    = $(this).parent().prev().data("theme-column")
-                var newColumnValue = $(this).siblings().find("input").val()
+                $("input").on("keydown", function(key) {
+                    if (key.keyCode === 13) {
+                        var themeID        = $("body").data("theme-id")
+                        var themeColumn    = $(this).parent().parent().prev().data("theme-column")
+                        var newColumnValue = $(this).val()
 
-                if (newColumnValue === ""){
-                  return;
-                } else {
-                    $.ajax({
-                        url: "/themes/agency/" + themeID,
-                        type: "PUT",
-                        data: {agency: { [themeColumn]: newColumnValue} },
-                        dataType: "json",
-                        success: function(result) {
-                            event.stopPropagation()
-                            console.log("success!", result.results)
-                            $(".form-inline").remove()
-                        }
-                    })
-                }
+                        editTextElement(themeID, themeColumn, newColumnValue)
+                    }
+                })
 
-            })
+                $(".btn-danger").on("click", function() {
+                    console.log(".btn-danger delete")
+                    $(clickedElement).text($(this).data('original-content'))
+                    $('.form-inline').remove()
+                })
+
+                // $("input.form-control").on("click", function() {
+                //     console.log("clicked inside input field")
+                //     event.stopPropagation()
+                // })
+
+                $(".btn-success").on("click", function() {
+                    console.log(".btn-success submit")
+                    var themeID        = $("body").data("theme-id")
+                    var themeColumn    = $(this).parent().prev().data("theme-column")
+                    var newColumnValue = $(this).siblings().find("input").val()
+
+                    editTextElement(themeID, themeColumn, newColumnValue)
+                })
+            }
+
+            // console.log(eventTargetTag, event.target.textContent, "text element!")
+        } else if (eventTargetTag === "IMG" || event.target.id === "edit-background-image") {
+
+            // console.log(eventTargetTag, event.target, "image or backgournd-image icon")
+        } else if (event.target.tagName === "I") {
+
+            // console.log(eventTargetTag, event.target, "font-icon")
         }
     })
+
+
+    // // Uploading Images
+    // var headerImageURL = $("header").data("header-image-url")
+    // $("header").css("background-image", "url("+headerImageURL+")")
+
+
+    // $(".click-to-upload").on("click", function(){
+    //     console.log("click-to-upload image")
+    //     var targetColumn = $(this).data("theme-column")
+
+    //     $(this).after("<form id='image-upload' enctype='multipart/form-data' action='/themes/agency/1' accept-charset='UTF-8' method='post'><input name='utf8' type='hidden' value='✓'><input type='hidden' name='_method' value='put'><input type='hidden' name='authenticity_token' value='g2i18zLyycMN3T5WTVJLZzcYOqFHbMsPrzkj3evOUWObohATRAxsfREBy/wfDwokeqhy3Z8zcJxqJOV91+M0EQ=='><input id='file' type='file' name='agency["+targetColumn+"]'><br><input type='submit' value='submit' disabled class='btn btn-primary'><button name='button' type='button' class='btn btn-danger'>Cancel</button></form>")
+
+    //     $(".btn-danger").on("click", function() {
+    //         console.log(".btn-danger for image-upload")
+    //         $('#image-upload').remove()
+    //     })
+
+    //     $('input:file').change(function() {
+    //         console.log("make sure user chooses file")
+    //         if ($(this).val()) {
+    //             $('input:submit').attr('disabled',false);
+    //         }
+    //     });
+    // })
 })
