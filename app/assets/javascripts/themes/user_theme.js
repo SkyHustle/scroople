0af7c722d6ee9ad98c8958658b83a3642ecfc4e5
Dmitry Sky <dmitry.sky@icloud.com>
2016-06-20 13:00:17 -0600

    remove click event listener from div elements and allow user to update their themes icons on the client side

--- app/assets/javascripts/themes/user_theme.js
+++ app/assets/javascripts/themes/user_theme.js
@@ -18,7 +18,7 @@ var editTextElement = function (themeID, themeColumn, newColumnValue) {
 }
 
 var editImageElement = function(targetColumn) {
-    $("body").append("<div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-body'><form id='image-upload' enctype='multipart/form-data' action='/themes/agency/1' accept-charset='UTF-8' method='post'><input name='utf8' type='hidden' value='✓'><input type='hidden' name='_method' value='put'><input type='hidden' name='authenticity_token' value='g2i18zLyycMN3T5WTVJLZzcYOqFHbMsPrzkj3evOUWObohATRAxsfREBy/wfDwokeqhy3Z8zcJxqJOV91+M0EQ=='><input id='file' type='file' name='agency["+targetColumn+"]'><br><input type='submit' value='submit' disabled class='btn btn-primary'><button data-toggle='modal' data-target='#myModal' name='button' type='button' class='btn btn-danger'>Cancel</button></form></div></div></div></div>")
+    $("body").append("<div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-body'>"+targetColumn+"<form id='image-upload' enctype='multipart/form-data' action='/themes/agency/1' accept-charset='UTF-8' method='post'><input name='utf8' type='hidden' value='✓'><input type='hidden' name='_method' value='put'><input type='hidden' name='authenticity_token' value='g2i18zLyycMN3T5WTVJLZzcYOqFHbMsPrzkj3evOUWObohATRAxsfREBy/wfDwokeqhy3Z8zcJxqJOV91+M0EQ=='><input id='file' type='file' name='agency["+targetColumn+"]'><br><input type='submit' value='submit' disabled class='btn btn-primary'><button data-toggle='modal' data-target='#myModal' name='button' type='button' class='btn btn-danger'>Cancel</button></form></div></div></div></div>")
 
 
     $('input:file').change(function() {
@@ -93,7 +93,7 @@ $(document).ready(function(){
 
     // element editing
     $("body").on("click", function() {
-        var textElements   = ["H1", "H2", "H3", "H4", "H5", "H6", "DIV", "P"]
+        var textElements   = ["H1", "H2", "H3", "H4", "H5", "H6", "P"]
         var eventTargetTag = event.target.tagName
 
         if ($.inArray(eventTargetTag, textElements) >= 0
@@ -110,15 +110,28 @@ $(document).ready(function(){
                 addTextEditForm(clickedElement, innerContent)
             }
 
-            // console.log(eventTargetTag, event.target.textContent, "text element!")
         } else if (eventTargetTag === "IMG" || event.target.id === "edit-background-image") {
             var targetColumn = event.target.dataset["themeColumn"]
             editImageElement(targetColumn)
 
-            // console.log(eventTargetTag, event.target, "image or backgournd-image icon")
         } else if (event.target.tagName === "I") {
+            if (event.target.dataset["themeColumn"]) {
+                targetIcon    = event.target.dataset["themeColumn"]
+                targetElement = event.target
 
-            // console.log(eventTargetTag, event.target, "font-icon")
+                $("body").append("<div class='portfolio-modal modal fade' id="+targetIcon+" tabindex='-1' role='dialog' aria-hidden='true'><div class='modal-content'><div class='close-modal' data-dismiss='modal'><div class='lr'><div class='rl'></div></div></div><div class='container'><div class='row'><div class='col-lg-8 col-lg-offset-2'><div class='modal-body'><div>Click To Select Icon</div><ul style='list-style: none;'><div class='row' id='first-row'><div class='container'><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-glass' data-unicode='f000'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-music' data-unicode='f001'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-search' data-unicode='f002'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-envelope-o' data-unicode='f003'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-heart' data-unicode='f004'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-star' data-unicode='f005'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-star-o' data-unicode='f006'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-user' data-unicode='f007'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-film' data-unicode='f008'></i></li><li class='col-md-1 col-sm-2 col-xs-6 grid-icon'><i class='fa fa-th-large' data-unicode='f009'></i></li></div></div></ul><button type='button' class='btn btn-primary' data-dismiss='modal'><i class='fa fa-times'></i>Close</button></div></div></div></div></div></div>'")
+
+                $("#" + targetIcon).modal("show")
+            } else {
+                var newIconClass = event.target.className
+                targetElement.className = newIconClass + " fa-stack-1x fa-inverse"
+                $("#" + targetIcon).modal("hide")
+            }
+
+
+
+
+            // console.log(eventTargetTag, event.target, event.target.textContent, event.target.parentElement.textContent)
         }
     })
 
