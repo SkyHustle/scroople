b7f17c0ca590aee2f021c4e49b5108d623827631
Dmitry Sky <dmitry.sky@icloud.com>
2016-07-05 13:48:45 -0700

    doin it and doin it and doin it well

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,4 @@
-user         = User.create(username: "dmitry", email: "dmitry@alkami.io", password: "alkami21")
-agency       = Agency.create(user_id: user.id)
+# user         = User.create(username: "dmitry", email: "dmitry@alkami.io", password: "alkami21")
+# agency       = Agency.create(user_id: user.id)
 
-# social_icon1 = SocialIcon.create(title: "fa fa-facebook", url: "https://www.facebook.com/alkami.io", user_id: user.id)
-# social_icon2 = SocialIcon.create(title: "fa fa-twitter", url: "https://twitter.com/AlkamiWeb", user_id: user.id)
-# social_icon3 = SocialIcon.create(title: "fa fa-linkedin", url: "https://www.linkedin.com/company/alkami", user_id: user.id)
-
-# social_icon4 = SocialIcon.create(title: "fa fa-facebook", url: "https://www.facebook.com/alkami.io", user_id: user.id)
-# social_icon5 = SocialIcon.create(title: "fa fa-twitter", url: "https://twitter.com/AlkamiWeb", user_id: user.id)
-# social_icon6 = SocialIcon.create(title: "fa fa-linkedin", url: "https://www.linkedin.com/company/alkami", user_id: user.id)
-
-# social_icon7 = SocialIcon.create(title: "fa fa-facebook", url: "https://www.facebook.com/alkami.io", user_id: user.id)
-# social_icon8 = SocialIcon.create(title: "fa fa-twitter", url: "https://twitter.com/AlkamiWeb", user_id: user.id)
-# social_icon9 = SocialIcon.create(title: "fa fa-linkedin", url: "https://www.linkedin.com/company/alkami", user_id: user.id)
-
-puts "#{user.username} successfully created along with #{agency}"
-# puts "#{user.social_icons}"
+# puts "#{user.username} successfully created along with #{agency}"
